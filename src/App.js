1f59955de05dd74a6248c08cd66c33841ca2a78e
rohit <you@example.com>
2023-07-27 00:05:21 +0530

    made fetch api to fetch news

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class App extends Component {
+const App = () => {
+  // state
+  const [news, setNews] = useState([]);
 
-  state = {
-    count: 0
-  }
+  // fetch news
+  const fetchNews = () => {
+    return fetch("http://hn.algolia.com/api/v1/search?query=react")
+      .then((result) => result.json())
+      .then((data) => setNews(data.hits))
+      .catch((error) => console.log(error));
+  };
 
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.increment}>
-         button clicked {this.state.count} times
-        </button>
-      </div>
-    )
+  useEffect(() => {
+    fetchNews();
+  }, []); // Empty dependency array to run the effect only once on mount
 
-  }
-}
+  return (
+    <div>
+      <h2>News</h2>
+      {news.map((n, i) => (
+        <p key={i}>{n.title}</p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+
+// class App extends Component {
+
+//   state = {
+//     count: 0
+//   }
+
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.increment}>
+//          button clicked {this.state.count} times
+//         </button>
+//       </div>
+//     )
+
+//   }
+// }
+
+// export default App;
